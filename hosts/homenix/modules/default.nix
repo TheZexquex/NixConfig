d46bcf507bba65641734e18d8c9910fee8de837c
TheZexquex <contact@thezexquex.dev>
2026-04-16 09:29:50 +0200

    fix: disable wireguard interface for discontinued project

--- hosts/homenix/modules/default.nix
+++ hosts/homenix/modules/default.nix
@@ -1,10 +1,10 @@
-{ ... }: {
+{...}: {
   imports = [
     ./firewall.nix
     ./wifi.nix
     ./bluetooth.nix
     ./boot.nix
     ./sunshine.nix
-    ./wireguard.nix
+    #    ./wireguard.nix
   ];
 }
